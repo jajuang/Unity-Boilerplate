d636c346ff1849a48f9ac4259864374218f5315e
J <jaj2585@gmail.com>
2018-01-26 03:28:42 -0800

    Initial commit

---
+++ Assets/_Game/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public static CameraController _instance;
+
+	public Transform TargetObject;
+	public bool IsMovingToNewTarget = false;
+	public float DampTime = 0; // The smoothening factor
+	private float OriginalDampTime; // The smoothening factor
+	public float DampTimeOnNewTarget = 0.22f; // The smoothening factor
+
+	public Vector3 CameraDistance;
+
+	public float MovementOffset = 0.2f; // Offset of how close the camera must be before it is "close enough" to reduce the damp time
+
+	private Vector3 velocity = Vector3.zero;
+	private Camera myCamera;
+
+	public float CameraHeightOffset = 0; // use negative numbers to zoom in and positive numbers to zoom out
+	public float CameraHeightOffsetOriginal = 0;
+
+
+	private void Awake()
+	{
+		_instance = this;
+
+		myCamera = gameObject.GetComponent<Camera>();
+		OriginalDampTime = DampTime;
+
+		Debug.Log(CameraDistance.x + "," + CameraDistance.y + "," + CameraDistance.z);
+		CameraDistance = myCamera.WorldToViewportPoint(TargetObject.position);
+	}
+
+
+	private void Update()
+	{
+		if (TargetObject)
+		{
+			Vector3 delta = TargetObject.position - myCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, CameraDistance.z));
+			Vector3 destination = transform.position + delta + new Vector3(0, CameraHeightOffset, 0);
+			Vector3 distanceToNewObj = (transform.position - destination);
+
+			if (IsMovingToNewTarget)
+			{
+				if (
+					(distanceToNewObj.x > MovementOffset) || (distanceToNewObj.x < (MovementOffset * -1)) ||
+					(distanceToNewObj.y > MovementOffset) || (distanceToNewObj.y < (MovementOffset * -1)) ||
+					(distanceToNewObj.z > MovementOffset) || (distanceToNewObj.z < (MovementOffset * -1))
+					) //((transform.position - destination) != Vector3.zero)
+				{
+					// Slow down the move speed for the transition
+					DampTime = DampTimeOnNewTarget;
+				}
+				else
+				{
+					IsMovingToNewTarget = false;
+					Debug.Log("Camera is now pointing at new object");
+					DampTime = OriginalDampTime;
+				}
+			}
+
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, DampTime);
+		}
+	}
+
+	//public void Update()
+	//{
+	//	transform.position = Vector3.Lerp(transform.position , TargetObject.transform.position + offset, Time.deltaTime *100);
+	//}
+
+
+	#region Custom functions
+
+	public void ChangeTarget(Transform newTarget, float heightOffset)
+	{
+		// Move the camera to to new playerobj
+		TargetObject = newTarget;
+
+		// TODO: Add special camera effects if there are any
+		Debug.Log("TODO: Add special camera effects if there are any");
+
+		ChangeCameraZoom(heightOffset);
+
+		// Set flag for damping
+		IsMovingToNewTarget = true;
+	}
+
+	public void ChangeCameraZoom(float heightOffset)
+	{
+		// Do zoom-in/out
+		CameraHeightOffset = CameraHeightOffsetOriginal + heightOffset;
+
+		// Set flag for damping
+		IsMovingToNewTarget = true;
+	}
+
+	#endregion
+}
---
+++ Assets/_Game/Scripts/Managers/MyGameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Enums for easy-to-rename buttton names
+public enum ButtonControlNames
+{
+	Start,
+	UseAction,
+	UseItem,
+	Interact
+}
+
+public enum GameMode
+{
+	NormalPlay,
+	StartMenu,
+	ClosedStartMenuBuffer, // HACK: to make sure we don't start a convo with a close-by npc on start menu "Continue" click
+	CombineMode
+}
+
+public class MyGameManager : MonoBehaviour
+{
+	#region variables
+	public static MyGameManager _instance;
+	public RuntimePlatform UserPlatform;
+
+	public Dictionary<ButtonControlNames, KeyCode> ButtonControls_Controller;
+	public Dictionary<ButtonControlNames, KeyCode> ButtonControls_Keyboard;
+	
+	public GameMode GameMode = GameMode.NormalPlay;
+	#endregion
+
+
+	void Awake()
+	{
+		_instance = this;
+		UserPlatform = Application.platform;
+
+		ButtonControls_Keyboard = new Dictionary<ButtonControlNames, KeyCode>();
+		ButtonControls_Keyboard[ButtonControlNames.Start] = KeyCode.Escape;
+		ButtonControls_Keyboard[ButtonControlNames.UseAction] = KeyCode.X;
+		ButtonControls_Keyboard[ButtonControlNames.UseItem] = KeyCode.Z;
+		ButtonControls_Keyboard[ButtonControlNames.Interact] = KeyCode.Space;
+		//ButtonControls_Keyboard["LeftBumper"] = KeyCode.L;
+		//ButtonControls_Keyboard["RightBumper"] = KeyCode.K;
+
+		ButtonControls_Controller = new Dictionary<ButtonControlNames, KeyCode>();
+		SetDefaultControllerValuesBasedOffOS(UserPlatform);
+	}
+
+	#region Controller and Keyboard Controls
+	
+	public void SetDefaultControllerValuesBasedOffOS(RuntimePlatform userOS)
+	{
+		if (userOS == RuntimePlatform.LinuxPlayer)
+		{
+			// TODO
+		}
+		else if (userOS == RuntimePlatform.OSXPlayer)
+		{
+			// TODO
+		}
+		else // Windows and everything else
+		{
+			ButtonControls_Controller[ButtonControlNames.Start] = KeyCode.Joystick1Button7; // Start Button
+			ButtonControls_Controller[ButtonControlNames.UseAction] = KeyCode.Joystick1Button1; // B Button
+			ButtonControls_Controller[ButtonControlNames.UseItem] = KeyCode.Joystick1Button2; // X Button
+			ButtonControls_Controller[ButtonControlNames.Interact] = KeyCode.Joystick1Button0; // A Button
+			//ButtonControls_Controller["LeftBumper"] = KeyCode.Joystick1Button4; // Left Bumper
+			//ButtonControls_Controller["RigthBumper"] = KeyCode.Joystick1Button5; // Right Bumper
+		}
+	}
+
+	#endregion
+
+	public void UpdateButtonControl(bool isController, ButtonControlNames btnName)
+	{
+		// TODO
+	}
+
+
+}
+
